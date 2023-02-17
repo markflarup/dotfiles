51113250022a1b4779506dd63b6a7f08c1b8b792
Mark Flarup-Jensen <mfj@stibodx.com>
2023-02-17 15:37:49 +0100

    Pins `transient` and `with-editor`

--- doom/packages.el
+++ doom/packages.el
@@ -18,6 +18,9 @@
 (package! ox-jira)
 (package! yaml-pro)
 
+;; https://github.com/doomemacs/doomemacs/issues/7078#issuecomment-1428775097
+(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
+(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
 ;; (package! github-theme)
 ;; (package! gruvbox-theme)
 
