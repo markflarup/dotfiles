c809e31c6dea98aacb569ab04fafbd03fef5d213
Mark Flarup-Jensen <mflarup@gmail.com>
2023-04-28 21:26:47 +0200

    Unpins `transient` and `with-editor`

--- doom/packages.el
+++ doom/packages.el
@@ -17,9 +17,6 @@
 (package! org-auto-tangle)
 (package! yaml-pro)
 
-;; https://github.com/doomemacs/doomemacs/issues/7078#issuecomment-1428775097
-(package! transient :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440")
-(package! with-editor :pin "391e76a256aeec6b9e4cbd733088f30c677d965b")
 ;; (package! github-theme)
 ;; (package! gruvbox-theme)
 
