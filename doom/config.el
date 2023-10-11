56882c511e3db7fe2d33524bd12ba9952b8e6d31
Mark Flarup-Jensen <mfj@stibodx.com>
2023-10-11 22:31:39 +0200

    Doom: Catppuccing > Gruvbox

--- doom/config.el
+++ doom/config.el
@@ -27,7 +27,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'gruvbox-light-medium)
+(setq doom-theme 'catppuccin)
+(setq catppuccin-flavor 'latte) ;; or 'frappe, 'macchiato, or 'mocha
 ;; (load-theme 'github t)
 
 
--- doom/packages.el
+++ doom/packages.el
@@ -20,7 +20,7 @@
 (package! restclient)
 
 ;; (package! github-theme)
-(package! gruvbox-theme)
+(package! catppuccin-theme)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
